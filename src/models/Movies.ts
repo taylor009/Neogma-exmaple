890ae2dff5c966aa9f39884c0d40bfb4174943d6
Taylor <taylor@baremetalcloud.com>
2021-02-02 00:34:16 -0500

    First Commit

---
+++ src/models/Movies.ts
@@ -0,0 +1,32 @@
+import { neogma } from "../app";
+
+import { ModelFactory, NeogmaInstance } from "neogma";
+
+export type MoviesPropertiesI = {
+    name: string;
+    year: number;
+}
+
+export interface MoviesRelatedNodesI {}
+
+export type MoviesInstance = NeogmaInstance<MoviesPropertiesI, MoviesRelatedNodesI>;
+
+export const Movies = ModelFactory<MoviesPropertiesI, MoviesRelatedNodesI>(
+    {
+        label: 'Movie',
+        primaryKeyField: 'name',
+        schema: {
+            name: {
+                type: 'string',
+                minLength: 1,
+                required: true,
+            },
+            year: {
+                type: 'number',
+                minimum: 1900,
+                required: true,
+            },
+        },
+    },
+    neogma,
+);
